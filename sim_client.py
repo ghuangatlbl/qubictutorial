b92d03563b5a0966592b3d5ae9f6f39ee4841df4
Neelay Fruitwala <neelay@lbl.gov>
2024-09-10 12:04:38 -0700

    update client, channel config

--- sim_client.py
+++ sim_client.py
@@ -2,6 +2,7 @@ import socket
 import pickle
 import numpy as np
 import matplotlib.pyplot as plt
+from distproc.executable import Executable
 
 RECV_BUFSIZE = 65536
 DAC_SAMPLE_DT = 1/8.e9 # 8 GHz DAC sample rate
@@ -25,21 +26,21 @@ class SimClient:
         self.host = host
         self.port = port
 
-    def run_program(self, asm_prog, sim_time, adc_stream=None, adc_delay=0, capture_demod=False):
+    def run_program(self, asm_prog: Executable, sim_time: float, 
+                    adc_stream: np.ndarray = None, adc_delay: float = 0):
         """
         Run a program in the cocotb/verilator simulator. Resulting DAC 
         output is stored in self.dacout (numpy array with shape (ndacs, nsamples)).
 
         Parameters
         ----------
-        asm_prog : dict
+        asm_prog : Executable
             compiled qubic program
         sim_time : float
             total simulation runtime (not including memory loads) in seconds
         """
         dumpdict = {'asm_prog': asm_prog, 
-                    'nsamples': sim_time//DAC_SAMPLE_DT,
-                    'capture_demod': capture_demod}
+                    'nsamples': sim_time//DAC_SAMPLE_DT}
         if adc_stream is not None:
             assert np.all(adc_stream <= 1)
             adc_stream *= (2**15 - 1)
@@ -68,9 +69,6 @@ class SimClient:
                 self.adc_timesteps = np.arange(0, self.dac_out.shape[1]*ADC_SAMPLE_DT, ADC_SAMPLE_DT)
                 self.acc = output_dict['acc']
                 self.adc_stream = adc_stream
-                if capture_demod:
-                    self.rdlo = output_dict['rdlo']
-                    self.rdlo_x_adc = output_dict['rdlo_x_adc']
 
     def plot_dac_out(self, channel, start_time=0, end_time=None):
         start_ind = int(start_time//DAC_SAMPLE_DT)
@@ -83,35 +81,5 @@ class SimClient:
         plt.xlabel('time (s)')
         plt.ylabel('DAC level')
         plt.title(f'DAC channel {channel}')
-        plt.show()
-
-    def plot_rdlo(self, channel, start_time, end_time=None):
-        start_ind = int(start_time//ADC_SAMPLE_DT)
-        if end_time is None:
-            end_ind = self.rdlo.shape[1]
-        else:
-            end_ind = int(end_time//ADC_SAMPLE_DT)
-
-        plt.plot(self.adc_timesteps[start_ind:end_ind], self.rdlo[channel, start_ind:end_ind].real/(2**15-1), label='I (real)')
-        plt.plot(self.adc_timesteps[start_ind:end_ind], self.rdlo[channel, start_ind:end_ind].imag/(2**15-1), label='Q (imag)')
-        plt.legend()
-        plt.xlabel('time (s)')
-        plt.ylabel('signal level')
-        plt.title(f'rdlo channel {channel}')
-        plt.show()
-
-    def plot_rdlo_x_adc(self, channel, start_time, end_time=None):
-        start_ind = int(start_time//ADC_SAMPLE_DT)
-        if end_time is None:
-            end_ind = self.rdlo_x_adc.shape[1]
-        else:
-            end_ind = int(end_time//ADC_SAMPLE_DT)
-
-        plt.plot(self.adc_timesteps[start_ind:end_ind], self.rdlo_x_adc[channel, start_ind:end_ind].real/(2**15-1), label='I (real)')
-        plt.plot(self.adc_timesteps[start_ind:end_ind], self.rdlo_x_adc[channel, start_ind:end_ind].imag/(2**15-1), label='Q (imag)')
-        plt.legend()
-        plt.xlabel('time (s)')
-        plt.ylabel('signal level')
-        plt.title(f'rdlo channel {channel}')
         plt.show()
 
